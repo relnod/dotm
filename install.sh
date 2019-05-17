a6462e6dd41f57f063b96deff48a263347c7674b
relnod <mail@paul-schiffers.de>
2019-05-17 13:26:16 +0200

    install.sh: make it work with release from goreleaser

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+install_dir="/usr/local/bin"
+bash_completion_dir="/etc/bash_completion.d"
+if [ "$1" = "--user" ]; then
+    install_dir="$HOME/.local/bin"
+    bash_completion_dir="$HOME/.bash_completion.d"
+fi
+
 get_latest_release() {
   curl --silent "https://api.github.com/repos/relnod/dotm/releases/latest" |
     grep '"tag_name":' |
@@ -12,7 +19,8 @@ target=""
 goarch=""
 
 case $(uname) in
-    "Linux")target=linux;;
+    "Linux")target=Linux;;
+    "Darwin")target=Darwin;;
     *)
         echo "Target $(uname) is not supported"
         exit 1
@@ -20,7 +28,8 @@ case $(uname) in
     esac
 
 case $(uname -m) in
-    "x86_64")goarch=amd64;;
+    "x86_64")goarch=x86_64;;
+    "i386")goarch=i386;;
     *)
         echo "Arch $(uname -a) is not supported"
         exit 1
@@ -33,29 +42,23 @@ if [ -x "$(command -v dotm)" ] && [ "$version" = "$(dotm --version | cut -d ' ' 
     exit
 fi
 
-name="dotm_${version}_${target}_${goarch}.tar.gz"
+# strip the "v" from the version
+rawversion="$(echo "$version" | cut -d "v" -f 2)"
+
+name="dotm_${rawversion}_${target}_${goarch}.tar.gz"
 
 echo "Downloading dotm binary at version ${version}"
 curl --silent -L "https://github.com/relnod/dotm/releases/download/${version}/${name}" -o "/tmp/${name}"
 
-install_dir="/usr/local/bin"
-bash_completion_dir="/etc/bash_completion.d"
-if [ "$1" = "--user" ]; then
-    install_dir="$HOME/.local/bin"
-    bash_completion_dir="$HOME/.bash_completion.d"
-fi
-if [ ! -d "$install_dir" ]; then
-    mkdir -p "$install_dir"
-fi
-if [ ! -d "$bash_completion_dir" ]; then
-    mkdir -p "$bash_completion_dir"
-fi
+[ ! -d "$install_dir" ] && mkdir -p "$install_dir"
+[ ! -d "$bash_completion_dir" ] && mkdir -p "$bash_completion_dir"
 
 echo "Installing dotm to $install_dir"
 if [ -f "$install_dir/dotm" ]; then
     rm -f "$install_dir/dotm"
 fi
-tar -C "$install_dir" -xzf "/tmp/${name}"
+
+tar -C "$install_dir" -xzvf "/tmp/${name}" dotm
 
 echo "Generating bash completions at $bash_completion_dir"
 dotm --genCompletions > "$bash_completion_dir/dotm"
