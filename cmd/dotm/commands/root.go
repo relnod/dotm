19fa5900182a8908cfc6b98fe265c44ebfd6a2a2
relnod <mail@paul-schiffers.de>
2019-06-03 14:30:06 +0200

    cmd/root: bump version

--- cmd/dotm/commands/root.go
+++ cmd/dotm/commands/root.go
@@ -115,7 +115,7 @@ var rootCmd = &cobra.Command{
 	Use:          "dotm",
 	Short:        "Dotm is a dotfile manager",
 	Long:         rootHelp,
-	Version:      "v0.4.0",
+	Version:      "v0.5.0",
 	SilenceUsage: true,
 	Args:         cobra.ExactArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
